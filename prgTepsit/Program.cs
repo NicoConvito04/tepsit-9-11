b0db0bc535fa499458eb9bc9b5f37af5bacaa259
Nico <nicoconvito04@gmail.com>
2020-11-05 11:40:54 +0100

    Dimensione immagine

--- prgTepsit/Program.cs
+++ prgTepsit/Program.cs
@@ -6,7 +6,18 @@ namespace prgTepsit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci il primo valore");
+            int d1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo valore");
+            int d2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci la dimensione in cui vuoi convertire l'immagine");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int valoreConverito;
+            valoreConverito = ((d1 * d2 * 3) / n);
+
+            Console.WriteLine($"la dimensione dell'immagine è {valoreConverito}");
+            Console.ReadLine();
+            
         }
     }
 }
